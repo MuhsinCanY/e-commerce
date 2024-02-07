ff20f093e881e070db77a7bdcdcab9d95258374c
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-02-07 14:06:15 +0300

    issue #1 to #7 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import axios from 'axios'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const instance = axios.create({
+    baseURL: 'https://fakestoreapi.com/products',
+    timeout: 1000,
+    headers: { 'X-Custom-Header': 'foobar' },
+  })
+
+  useEffect(() => {
+    instance
+      .get()
+      .then(function (response) {
+        console.log(response.data)
+        setProducts(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+  }, [])
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Products</h1>
+      {products.map((p) => {
+        return <li key={p.id}>{p.title}</li>
+      })}
     </>
   )
 }
